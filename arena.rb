4bb549ab5c1ee58ec4ef1af8cf160d0c1bdd9bef
Sven A. Schmidt <sas@finestructure.co>
2020-02-28 18:35:00 +0100

    arena-0.8.1

--- arena.rb
+++ arena.rb
@@ -1,11 +1,15 @@
 class Arena < Formula
+    VERSION = "0.8.1"
+    sha256 "7f783b93fb7be2fd442d190c0b5853b3cd8dea637b7539b40bf7bf0feefc573e"
+
     desc "Describe your project"
     homepage "https://github.com/finestructure/Arena"
-    url "https://github.com/finestructure/Arena/archive/0.8.0.tar.gz"
+    url "https://github.com/finestructure/Arena/archive/#{VERSION}.tar.gz"
     
-    sha256 "fe2f27db552bcda40c035b0f46f61079d40f14a05e32ad080f7cff52ea071f72"
-
     def install
+      # write version to file (for tool version reporting)
+      File.write('Sources/ArenaCore/Version.swift', "public let ArenaVersion = \"#{VERSION}\"")
+
       system "swift", "build",
           "--configuration", "release",
           "--disable-sandbox"
