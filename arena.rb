5fed8a003f6906cd02e0f6e5127cee3537685d66
Sven A. Schmidt <sas@finestructure.co>
2020-03-02 15:07:37 +0100

    arena-0.9.1

--- arena.rb
+++ arena.rb
@@ -5,8 +5,8 @@
 # - update sha256, confirm update works and push
 
 class Arena < Formula
-    VERSION = "0.9.0"
-    sha256 "8d22bfaf06b4e82c82c1f1a915d615d2571d473da0d7edcbdc763a5ea0b04b18"
+    VERSION = "0.9.1"
+    sha256 "d6933eb13da1a32853f8fffb6e45d27c01f01848307fab46ec9d25c5f3e2dcdd"
 
     desc "Describe your project"
     homepage "https://github.com/finestructure/Arena"
